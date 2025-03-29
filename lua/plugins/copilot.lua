e69133cecde7ac11bef97a4ee929a81932e38ce3
tommy <tomsonfour@gmail.com>
2025-03-29 14:16:01 +0100

    add: copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,24 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter',
     config = function()
-      require("copilot").setup {
+      require('copilot').setup {
         suggestion = { enabled = false },
         panel = { enabled = false },
       }
     end,
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }
