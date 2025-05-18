3892b5ec8779b355c8e1bd06f8e9da181ace61ad
tommy <tomsonfour@gmail.com>
2025-05-19 00:09:12 +0200

    feature: add keybind to <leader>C launch telescope with CopilotChat*

--- init.lua
+++ init.lua
@@ -991,3 +991,4 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,7 +2,15 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 return {
+  map('n', '<leader>C', function()
+    require('CopilotChat')
+    require('telescope.builtin').commands {
+      prompt_title = 'CopilotChat Commands',
+    }
+  end, { desc = 'CopilotChat Commands' }),
 
+  -- close buffer
+  map('n', '<leader>x', '<cmd>BufferClose<CR>', { desc = 'close buffer' }),
   -- tabs (barbar)
   -- Move to previous/next
   map('n', '<Tab>', '<Cmd>BufferPrevious<CR>', opts),
